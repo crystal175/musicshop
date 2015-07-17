752a6e217c82c02e5cdaf1c758d1959bc4a6f75d
crystal175 <crystalman87@gmail.com>
2015-07-17 23:03:03 +0300

    Added artist, song models

---
+++ search/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Artist',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=30, db_index=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Song',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('title', models.CharField(unique=True, max_length=50, db_index=True)),
+                ('name', models.ForeignKey(to='search.Artist')),
+            ],
+        ),
+    ]
--- search/models.py
+++ search/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=30, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Song(models.Model):
+    name = models.ForeignKey(Artist)
+    title = models.CharField(max_length=50, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.title
+
